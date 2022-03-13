5e9a2429ad6522829a11ca2058d17c7892a4bfd0
sdltsky <sedletskiyna@mail.ru>
2022-03-13 15:43:41 +0500

    Повесил на кнопку clickListener

--- app/src/main/java/ru/gb/kotlinlesson1/MainActivity.kt
+++ app/src/main/java/ru/gb/kotlinlesson1/MainActivity.kt
@@ -2,11 +2,19 @@ package ru.gb.kotlinlesson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import ru.gb.kotlinlesson1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonExample = findViewById<Button>(R.id.button) as Button
+        buttonExample.setOnClickListener {
+            Toast.makeText(this, "Test Click", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
